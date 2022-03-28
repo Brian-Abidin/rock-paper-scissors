5616065f499e3e0e7dcabc7bec49f0133b74668c
Brian Abidin <Brian.abidin1@gmail.com>
2022-03-28 17:14:56 -0400

    fixed countergoing past 5 wins/loss

--- rps.js
+++ rps.js
@@ -17,46 +17,59 @@ let computerPlay = () => word[Math.floor(Math.random()*word.length)]; // create 
 let win = 0;
 let loss = 0;
 function playRound(playerSelection, computerSelection){ // create function takes 2 parameters of user input RPS vs Computer RPS
-while(playerSelection) { 
-  const round = document.querySelector('#round');
-  if (playerSelection === 'Rock' && computerSelection === 'Paper'){ // create if statements comparing all outcomes of RPS for computer and User
-    round.textContent = "You Lose! " + computerSelection + " beats " + playerSelection + "!"; // return the winner using a string "win/lose x beats y"
-    loss +=1;
-    document.getElementById("loss").textContent = loss;
-    return 0; //returns false if lose
-  } else if (playerSelection === 'Paper' && computerSelection === 'Scissors'){
-    round.textContent = "You Lose! " + computerSelection + " beats " + playerSelection + "!";
-    loss +=1;
-    document.getElementById("loss").textContent = loss;
-    return 0;
-  } else if (playerSelection === 'Scissors' && computerSelection === 'Rock'){
-    round.textContent = "You Lose! " + computerSelection + " beats " + playerSelection + "!";
-    loss +=1;
-    document.getElementById("loss").textContent = loss;
-    return 0;
-  } else if (playerSelection === 'Rock' && computerSelection === 'Scissors'){
-    round.textContent = "You Win! " + playerSelection + " beats " + computerSelection + "!";   
-    win +=1;
-    document.getElementById("win").textContent = win;
-    return 1; //returns true if win
-  } else if (playerSelection === 'Paper' && computerSelection === 'Rock'){
-    round.textContent = "You Win! " + playerSelection + " beats " + computerSelection + "!"; 
-    win +=1;
-    document.getElementById("win").textContent = win;
-    return 1;
-  } else if (playerSelection === 'Scissors' && computerSelection === 'Paper'){
-    round.textContent = "You Win! " + playerSelection + " beats " + computerSelection + "!"; 
-    win +=1;
-    document.getElementById("win").textContent = win;
-    return 1;
-  } else {
-    round.textContent = "It's a Draw.";
-    return;
+  while(playerSelection) { 
+    if(win === 5 || loss === 5){
+      break;
+    } else {
+    const round = document.querySelector('#round');
+    if (playerSelection === 'Rock' && computerSelection === 'Paper'){ // create if statements comparing all outcomes of RPS for computer and User
+      loss +=1;
+      document.getElementById("loss").textContent = loss;
+      if(loss === 5) break;
+      round.textContent = "You Lose! " + computerSelection + " beats " + playerSelection + "!"; // return the winner using a string "win/lose x beats y"
+      return 0; //returns false if lose
+    } else if (playerSelection === 'Paper' && computerSelection === 'Scissors'){
+      loss +=1;
+      document.getElementById("loss").textContent = loss;
+      if(loss === 5) break;
+      round.textContent = "You Lose! " + computerSelection + " beats " + playerSelection + "!";
+      return 0;
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Rock'){
+      loss +=1;
+      document.getElementById("loss").textContent = loss;
+      if(loss === 5) break;
+      round.textContent = "You Lose! " + computerSelection + " beats " + playerSelection + "!";
+      return 0;
+    } else if (playerSelection === 'Rock' && computerSelection === 'Scissors'){
+      win +=1;
+      document.getElementById("win").textContent = win;
+      if(win === 5) break;
+      round.textContent = "You Win! " + playerSelection + " beats " + computerSelection + "!";
+      return 1; //returns true if win
+    } else if (playerSelection === 'Paper' && computerSelection === 'Rock'){
+      win +=1;
+      document.getElementById("win").textContent = win;
+      if(win === 5) break;
+      round.textContent = "You Win! " + playerSelection + " beats " + computerSelection + "!"; 
+      return 1;
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Paper'){
+      win +=1;
+      document.getElementById("win").textContent = win;
+      if(win === 5) break;
+      round.textContent = "You Win! " + playerSelection + " beats " + computerSelection + "!"; 
+      return 1;
+    } else {
+      round.textContent = "It's a Draw.";
+      return;
+    }  
+  }
+  }
+  if(win === 5){
+    round.textContent = "You're the Rock, Paper, Scissors champion!";
+    }else if(loss === 5){
+    round.textContent = "Better luck next time...";
+    }
   } 
-}
-console.log("For not entering Rock, Paper, or Scissors, you lose.");
-return 0;
-}
 /*
  function game(){ // create function called game
   let win = 0;
@@ -88,7 +101,6 @@ const rock = document.querySelector('#rock'); //selecting first id of rock in ht
     let playerSelection = document.querySelector('#rock').textContent;
     let computerSelection = computerPlay(word);
     playRound(playerSelection,computerSelection);
-    console.log(playerSelection);
 }); 
 
 const paper = document.querySelector('#paper');
@@ -96,7 +108,6 @@ const paper = document.querySelector('#paper');
     let playerSelection = document.querySelector('#paper').textContent;
     let computerSelection = computerPlay(word);
     playRound(playerSelection,computerSelection);
-    console.log(playerSelection);
 }); 
 
 const scissors = document.querySelector('#scissors');
@@ -104,5 +115,4 @@ const scissors = document.querySelector('#scissors');
     let playerSelection = document.querySelector('#scissors').textContent;
     let computerSelection = computerPlay(word);
     playRound(playerSelection,computerSelection);
-    console.log(playerSelection);
 }); 
